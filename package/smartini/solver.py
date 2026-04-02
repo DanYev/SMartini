75366127d6ed0b6796f97bd3f478a46e9f16e238
Dan Y <dyangali@asu.edu>
2026-04-02 11:54:28 -0700

    work

--- package/smartini/solver.py
+++ package/smartini/solver.py
@@ -1,36 +1,7 @@
-r"""
-Created on March 13, 2019 by Andrew Abi-Mansour
-Updated to Martini 3 force field on January 31, 2025 by Magdalena Szczuka
-
-This is the::
-    _   _   _ _____ ___     __  __    _    ____ _____ ___ _   _ ___   __  __ _____
-   / \ | | | |_   _/ _ \   |  \/  |  / \  |  _ \_   _|_ _| \ | |_ _|  |  \/  |___ /  
-  / _ \| | | | | || | | |  | |\/| | / _ \ | |_) || |  | ||  \| || |   | |\/| | |_ \  
- / ___ \ |_| | | || |_| |  | |  | |/ ___ \|  _ < | |  | || |\  || |   | |  | |___) | 
-/_/  _\_\___/  |_| \___/   |_|  |_/_/   \_\_| \_\|_| |___|_| \_|___|  |_|  |_|____/    
-                                                
-
-A tool for automatic MARTINI 3 force field mapping and parametrization of small organic molecules
-
-Developers::
-        Magdalena Szczuka (magdalena.szczuka at univ-tlse3.fr)
-        Tristan BEREAU (bereau at mpip-mainz.mpg.de)
-        Kiran Kanekal (kanekal at mpip-mainz.mpg.de)
-        Andrew Abi-Mansour (andrew.gaam at gmail.com)
-
-AUTO_MARTINI M3 is open-source, distributed under the terms of the GNU Public
-License, version 2 or later. It is distributed in the hope that it will
-be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
-received a copy of the GNU General Public License along with PyGran.
-If not, see http://www.gnu.org/licenses . See also top-level README
-and LICENSE files.
-""" 
-
-from . import output
-from . import partitioning
+import requests
+from bs4 import BeautifulSoup
+from . import partitioning, output
 from .topology import Topology, run_bartender
-from .common import *
 
 logger = logging.getLogger(__name__)
 
