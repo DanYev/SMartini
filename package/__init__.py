e58c71ef5984b53f62edf821b44beeaea301931b
DanYev <dan1054yang@gmail.com>
2026-08-18 08:06:31 -0700

    restructured main

---
+++ package/__init__.py
@@ -0,0 +1,25 @@
+import logging
+import os
+
+
+def setup_logging(level=logging.INFO, force=False):
+        """Configure root logging for scripts using smartini.
+
+        By default, this only configures logging when no handlers are present.
+        Set ``force=True`` to override existing handlers.
+        """
+        root_logger = logging.getLogger()
+        if force or not root_logger.handlers:
+                logging.basicConfig(
+                        level=level,
+                        format="%(levelname)s [%(filename)s:%(lineno)d] %(message)s",
+                        force=force,
+                )
+        else:
+                root_logger.setLevel(level)
+
+from ..package.smartini import solver, topology
+from ._version import __version__
+
+
+__all__ = ["solver", "topology", "partitioning", "__version__", "setup_logging"]
